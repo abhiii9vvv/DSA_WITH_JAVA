36db27f87cb907148d5b12219093e7fe5e2ca8c8
ABHINAV TIWARY <gyanutiwari758@gmail.com>
2025-06-21 19:52:02 +0530

    Array Reverse

--- First/src/ReverseArray.java
+++ First/src/ReverseArray.java
@@ -1,5 +1,14 @@
 package First.src;
 
 public class ReverseArray {
+    static void reverse(int[] nums){
+        for (int i = nums.length; i <0 ; i--) {
+            System.out.println(nums[i]);
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        reverse(arr);
+    }
 }
