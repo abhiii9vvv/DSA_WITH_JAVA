3b5eba9e45db4a71d86e0128fee853ed3b0c2ec9
ABHINAV TIWARY <gyanutiwari758@gmail.com>
2025-06-22 00:31:38 +0530

    Add ReverseArray.java with reverse logic using extra array

--- First/src/ReverseArray.java
+++ First/src/ReverseArray.java
@@ -11,6 +11,24 @@ public class ReverseArray {
         return ans;
     }
 
+    static void swapInArray(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    static void reverseAdvance(int[] nums){
+        int i=0,j=nums.length-1;
+
+        while (i<j){
+            swapInArray(nums,i,j);
+            i++;
+            j--;
+        }
+
+    }
+
+
     static void print(int[] nums){
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i]+" ");
@@ -18,8 +36,11 @@ public class ReverseArray {
     }
 
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5};
-        int[] reversed=reverse(arr);
-        print(reversed);
+//        int[] arr={1,2,3,4,5};
+        int[] arr2={5,4,3,2,1,3};
+//        int[] reversed=reverse(arr);
+//        print(reversed);
+        reverseAdvance(arr2);
+        print(arr2);
     }
 }
