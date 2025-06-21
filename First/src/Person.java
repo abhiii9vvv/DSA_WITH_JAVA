2e7a07847a4bb5b529aca9c0ba61fa0e9d8be94c
ABHINAV TIWARY <gyanutiwari758@gmail.com>
2025-06-21 19:36:23 +0530

    Added Swap2Values.java with swapWithoutTemp method

---
+++ First/src/Person.java
@@ -0,0 +1,10 @@
+package First.src;
+
+public class Person {
+    int age=20;
+
+    public static void main(String[] args) {
+        Person Rohan= new Person();
+        System.out.println(Rohan.age);
+    }
+}
