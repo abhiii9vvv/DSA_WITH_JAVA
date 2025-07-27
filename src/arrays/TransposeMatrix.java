28efc7233c046811149a27409e3961bacf7c959c
ABHINAV TIWARY <gyanutiwari758@gmail.com>
2025-07-27 16:43:00 +0530

    Add program to compute transpose of a 2D matrix

--- src/arrays/TransposeMatrix.java
+++ src/arrays/TransposeMatrix.java
@@ -1,22 +1,46 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class TransposeMatrix {
     public static void main(String[] args) {
-        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
-        int[][] transpose= new int[arr[0].length][arr.length];
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                transpose[j][i]=arr[i][j];
+        // Taking input for matrix dimensions
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] arr = new int[rows][cols];
+
+        // Taking input for matrix elements
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
 
+        int[][] transpose = new int[cols][rows];
+
+        // Transposing the matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = arr[i][j];
+            }
+        }
+
+        // Printing the transpose
+        System.out.println("Transpose of the matrix:");
         for (int i = 0; i < transpose.length; i++) {
             for (int j = 0; j < transpose[0].length; j++) {
-                System.out.print(transpose[i][j]+" ");
+                System.out.print(transpose[i][j] + " ");
             }
             System.out.println();
         }
-    }
 
+        sc.close();
+    }
 }
