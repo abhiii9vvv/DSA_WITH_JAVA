9fee5dd45f8bff0bfe1da86868bd9da1c4947769
ABHINAV TIWARY <gyanutiwari758@gmail.com>
2025-07-27 17:40:09 +0530

    Add in-place 90-degree clockwise rotation for square matrix

---
+++ src/arrays/RotateAt90.java
@@ -0,0 +1,46 @@
+package arrays;
+
+public class RotateAt90 {
+    public static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rotate(int[][] arr){
+        int n= arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j <n ; j++) {
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int left=0,right=n-1;
+            while (left<right){
+                int temp=arr[i][left];
+                arr[i][left]=arr[i][right];
+                arr[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+
+        System.out.println("Original");
+        print2DArray(arr);
+
+        rotate(arr);
+
+        System.out.println("After Rotate");
+        print2DArray(arr);
+    }
+}
